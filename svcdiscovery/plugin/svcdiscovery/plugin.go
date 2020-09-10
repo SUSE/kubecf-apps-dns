6b50c048a57a8820135c10e985b87dd95f99f494
Thulio Ferraz Assis <thulio.assis@suse.com>
2020-09-10 16:34:18 -0500

    fix: debug level for logging
    Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

--- svcdiscovery/plugin/svcdiscovery/plugin.go
+++ svcdiscovery/plugin/svcdiscovery/plugin.go
@@ -104,7 +104,7 @@ func (sd *ServiceDiscovery) respond(rw dns.ResponseWriter, req *dns.Msg, ips []s
 	}
 	res.Answer = answer
 
-	sd.log.Infof("%s: %+v\n", qname, answer)
+	sd.log.Debugf("%s: %+v\n", qname, answer)
 
 	return rw.WriteMsg(res)
 }
